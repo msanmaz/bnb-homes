5104e7d95f4807d8d7edcfe9992e7b873ae6b1ba
msanmaz <osanmaz4992@gmail.com>
2022-10-24 20:10:49 +0100

    link rules

--- components/Navbar/side-bar.js
+++ components/Navbar/side-bar.js
@@ -8,11 +8,11 @@ const SideBar = ({isOpen}) => {
                 className={`transform top-0 right-0 w-64 bg-[#161616] fixed h-full overflow-auto ease-in-out rounded-l-xl transition-all duration-300 z-30"
                 ${isOpen ? 'translate-x-0 visible' : 'translate-x-full invisible'}`}>
                 <ul className="items-center justify-between text-base text-white pt-4 mt-48 ml-20 lg:pt-0">
-                    <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white" href="/">Home</a></li>
-                    <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white" href="/about">About Us</a></li>
-                    <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/contact">Contact</a></li>
-                    <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/portfoy">For Sale</a></li>
-                    <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/kiralik">For Rent</a></li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white" href="/">Home</li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white" href="/about">About Us</li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/contact">Contact</li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/portfoy">For Sale</li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" href="/kiralik">For Rent</li>
 
                 </ul>
             </aside>
