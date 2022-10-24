b240535f0638b45af81db297b3cfc44bc8e313bb
msanmaz <osanmaz4992@gmail.com>
2022-10-24 22:55:59 +0100

    multiplechild for sidebar link comp
    11

--- components/Navbar/side-bar.js
+++ components/Navbar/side-bar.js
@@ -8,11 +8,11 @@ const SideBar = ({isOpen}) => {
                 className={`transform top-0 right-0 w-64 bg-[#161616] fixed h-full overflow-auto ease-in-out rounded-l-xl transition-all duration-300 z-30"
                 ${isOpen ? 'translate-x-0 visible' : 'translate-x-full invisible'}`}>
                 <ul className="items-center justify-between text-base text-white pt-4 mt-48 ml-20 lg:pt-0">
-                   <Link href='/'> <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white">Home</li></Link>
-                   <Link href='/aboutus'> <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white">About Us</li></Link>
-                   <Link href='/contact'> <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2">Contact</li></Link>
-                   <Link href='/sale'> <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" >For Sale</li></Link>
-                   <Link href='/rental'><li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" >For Rent</li></Link>
+                   <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white"> <Link href='/'>Home</Link></li>
+                  <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white"> <Link href='/aboutus'> About Us</Link></li>
+                    <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2"><Link href='/contact'>Contact</Link></li>
+                   <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" ><Link href='/sale'> For Sale</Link></li>
+                  <li className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white lg:mb-0 mb-2" > <Link href='/rental'>For Rent</Link></li>
 
                 </ul>
             </aside>
