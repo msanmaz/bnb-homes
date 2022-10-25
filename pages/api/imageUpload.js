4b59a69692ef3010a0c7fccce64fd9f75780105e
msanmaz <osanmaz4992@gmail.com>
2022-10-25 19:21:34 +0100

    closed header

--- pages/api/imageUpload.js
+++ pages/api/imageUpload.js
@@ -1,40 +1,42 @@
 import prisma from 'lib/prisma'
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST' && req.method !=='DELETE') {
+  if (req.method !== 'POST' && req.method !== 'DELETE') {
     return res.status(501).end()
   }
 
-  const { image,id,imageId} = req.body
+  const { image, id, imageId } = req.body
   console.log(req.body)
-  if (req.method === 'POST') { 
+  if (req.method === 'POST') {
     const imgUpload = await prisma.Images.create({
-      data:{
-        url:image,
-        imageque:imageId,
-        house:{
-          connect:{
+      data: {
+        url: image,
+        imageque: imageId,
+        house: {
+          connect: {
             id: id
           }
         }
       }
     })
-    if(imgUpload){
-      res.json({success:'All Images Uplaoded!'})
+    if (imgUpload) {
+      res.json({ success: 'All Images Uplaoded!' })
+      res.end()
+    } else {
+      res.json({ success: 'Encountered With An Error!' })
+      res.end()
+
+    }
+
+  }
+
+
+  if (req.method === 'DELETE') {
+    const del = await prisma.Images.deleteMany({})
+    if (del) {
+      res.send(del)
       res.end()
     }
-    res.json({success:'Encountered With An Error!'})
-    res.end()
-
-  }
-
-
-  if(req.method === 'DELETE'){
-  const del =  await prisma.Images.deleteMany({})
-  if(del){
-    res.send(del)
-    res.end()  
-  }
 
   }
 }
